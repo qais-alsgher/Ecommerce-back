22bba11d2cbd78d0e97a2546ca7bebdae521eccd
qais-alsgher <qaisalsgher@gmail.com>
2023-06-29 08:10:18 +0000

    fix error and added bearerAuth to 90% of routs

--- routes/wishlist.routes.js
+++ routes/wishlist.routes.js
@@ -9,8 +9,8 @@ const {
 } = require("../controllers/wishlistController");
 const bearerAuth = require("../middlewares/bearer-auth");
 
-router.get("/wishlist", getWishlist);
-router.get("/wishlist/:id", getUserWishlist);
+router.get("/wishlist", bearerAuth, getWishlist);
+router.get("/wishlist/:id", bearerAuth, getUserWishlist);
 router.post("/wishlist", bearerAuth, createWishlist);
 router.delete("/wishlist/:id", bearerAuth, deleteWishlist);
 router.put("/wishlist/:id", bearerAuth, updateWishlist);
