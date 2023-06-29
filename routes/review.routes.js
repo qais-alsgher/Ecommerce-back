41d1fd66ddc1f8467083310b8dfe1c10de37e370
qais-alsgher <qaisalsgher@gmail.com>
2023-06-29 12:47:36 +0000

    add acl to admin routes and some ather roues and include reporte model in the users

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -8,11 +8,12 @@ const {
   updateReview,
 } = require("../controllers/reviewController");
 const bearerAuth = require("../middlewares/bearer-auth");
+const acl = require("../middlewares/acl");
 
 router.get("/review", getReview);
 router.get("/review/:id", getItemReview);
 router.post("/review", bearerAuth, createReview);
-router.delete("/review/:id", bearerAuth, deleteReview);
+router.delete("/review/:id", bearerAuth, acl, deleteReview);
 router.put("/review/:id", bearerAuth, updateReview);
 
 module.exports = router;
